97bbe3d813425221e0d3b54801fbc6970a4150da
martyall <martin.allen26@gmail.com>
2015-09-17 02:54:44 -0400

    bad now works but is really slow

--- src/Pear/Integer.hs
+++ src/Pear/Integer.hs
@@ -42,8 +42,8 @@ lexeme :: Parser a -> Parser a
 lexeme p = whitespace *> p <* whitespace
 
 plus, minus, times, divide, exponnt :: Parser (Binary LExp)
-plus = (reservedOp "+") >> (return $ Binary (BinaryExp Plus) 0 L)
-minus = (reservedOp "-") >> (return $ Binary (BinaryExp Minus) 0 L)
+plus = (reservedOp "-->") >> (return $ Binary (BinaryExp Plus) 2 L)
+minus = (reservedOp "--") >> (return $ Binary (BinaryExp Minus) 0 L)
 times = (reservedOp "*") >> (return $ Binary (BinaryExp Times) 1 L)
 divide = (reservedOp "/") >> (return $ Binary (BinaryExp Divide) 1 L)
 exponnt = (reservedOp "^") >> (return $ Binary (BinaryExp Exponent) 2 R)
--- src/Pear/bad.hs
+++ src/Pear/bad.hs
@@ -6,53 +6,47 @@ import Text.Parsec
 import Text.Parsec.String (Parser)
 
 
-data Binary a = Binary {
-  op :: Parser (a->a->a)
-  , associativity :: Associativity
-}
+data Binary a = Binary { op :: Parser (a -> a-> a)
+                       , assoc :: Associativity
+                       }
 
-data Associativity = L | R
+data Associativity = L | R deriving (Show, Eq)
 
 type Symbol a = Parser a
 
-data Algebra a = Algebra {
-  binaries :: [Binary a]
-  , symbols :: [Symbol a]
-}
+data Algebra a = Algebra { binaries :: [Binary a]
+                         , symbols :: [Symbol a]
+                         }
 
-expression :: Algebra a -> Parser a
-expression a = stateful_expression a 0
 
-stateful_expression :: Algebra a -> Int -> Parser a
-stateful_expression a i = parse_binary a i <|> atom a
+expression :: Algebra a -> Algebra a -> Parser a
+expression curr orig = case (binaries curr) of
+  [] -> try ((choice . symbols) orig) <|> (parens $ expression orig orig)
+  otherwise -> case (assoc . head . binaries) curr of
+    L -> leftBinary curr orig
+    R -> rightBinary curr orig
 
-atom :: Algebra a -> Parser a
-atom a = try (choice . symbols $ a) <|> parens (expression a)
+leftBinary :: Algebra a -> Algebra a -> Parser a
+leftBinary curr orig = try applyOpL
+                    <|> expression nextAlg orig
+  where
+    nextAlg = curr { binaries = (tail . binaries) curr }
+    applyOpL = do
+      arg1 <- expression nextAlg orig
+      op <- (op . head . binaries) curr
+      arg2 <- expression curr orig
+      return $ op arg1 arg2
 
-simple_binary :: Algebra a -> Int -> Parser a
-simple_binary a i = do
-  arg1 <- (atom a)
-  op <- getop a i
-  arg2 <- stateful_expression a i
-  return $ op arg1 arg2
+rightBinary :: Algebra a -> Algebra a -> Parser a
+rightBinary curr orig = (try ((expression nextAlg orig) >>= (doA curr orig)))
+                      <|> (expression nextAlg orig)
+  where
+    nextAlg = curr { binaries = (tail . binaries) curr }
+    doA curr orig arg1 = do
+      op <- (op . head . binaries) curr
+      arg2 <- try ((expression nextAlg orig) >>= (doA curr orig)) <|> (expression nextAlg orig)
+      return $ op arg1 arg2
 
--- Sorry about this long function...
--- Split it up if you want
-parse_binary :: Algebra a -> Int -> Parser a
-parse_binary a i
-  | i == ((length $ binaries a) - 1) = simple_binary a i
-  | otherwise = inner_parse <|> parse_binary a (i + 1)
-    where
-      inner_parse = do
-        arg1 <- parse_binary a (i + 1)
-        op <- getop a i
-        arg2 <- parse_binary a i
-        return $ op arg1 arg2
-
-getop :: Algebra a -> Int -> Parser (a->a->a)
-getop a i
-  | (length . binaries $ a) > i = op $ (binaries a) !! i
-  | otherwise = parserZero
 
 
 
@@ -78,11 +72,18 @@ data LExp
   deriving (Show)
 
 
-plus, minus, times, divide, exponnt :: [Binary LExp]
-plus = (reservedOp "+") >> (return $ Binary (BinaryExp Plus) L)
-minus = (reservedOp "-") >> (return $ Binary (BinaryExp Minus) L)
-times = (reservedOp "*") >> (return $ Binary (BinaryExp Times) L)
-divide = (reservedOp "/") >> (return $ Binary (BinaryExp Divide) L)
-exponnt = (reservedOp "^") >> (return $ Binary (BinaryExp Exponent) R)
+plus, minus, times, divide, exponnt :: Parser (LExp -> LExp -> LExp)
+plus = (reservedOp "+") >> (return $ BinaryExp Plus)
+minus = (reservedOp "-") >> (return $ BinaryExp Minus)
+times = (reservedOp "*") >> (return $ BinaryExp Times)
+divide = (reservedOp "/") >> (return $ BinaryExp Divide)
+exponnt = (reservedOp "^") >> (return $ BinaryExp Exponent)
 
+intelel = integer >>= return . Const . LInt
 
+bs = [Binary plus L, Binary minus L, Binary times L, Binary divide L, Binary exponnt R]
+sys = [intelel]
+
+algebra = Algebra bs sys
+
+lparse = parse (expression algebra algebra) ""
