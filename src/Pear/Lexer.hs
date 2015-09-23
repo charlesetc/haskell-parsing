305a384a1fca68b4fc703eb08102196e5f2a5199
Charles Chamberlain <cchamberlain@uchicago.edu>
2015-09-22 20:59:38 -0400

    Better dependancies

--- src/Pear/Lexer.hs
+++ src/Pear/Lexer.hs
@@ -1,22 +1,14 @@
 module Pear.Lexer where
 
 import Prelude hiding (lex)
-import Text.Parsec.String (Parser)
 
 import Pear.Operator
-import Pear.Operator.Algebra
-import Pear.Operator.Stack
-import Pear.Operator.Lexer
-import Pear.Operator.Tree
-
-import Pear.Operator.Concrete
 import Pear.Types
 
-import System.IO
-import Control.Monad.Reader
-import Control.Monad.State.Lazy
+import Text.Parsec
+import Text.Parsec.String
 
-import Text.Parsec (parse, ParseError)
-
+parseEverything :: Parser Ast
 parseEverything = pearAlgebra
+
 lex = parse (parseEverything) ""
